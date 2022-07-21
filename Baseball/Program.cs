6590b2621c4512a2115c61f8581f118d0e754a40
JongSeung Kim <jngsngkm@gmail.com>
2022-07-21 22:29:56 +0900

    strike, ball count

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -14,6 +14,11 @@ namespace Baseball
             Console.WriteLine("=========================BASEBALL=========================");
             Console.WriteLine("==========================================================");
 
+            Console.WriteLine("NUMBERS");
+            int number1 = 3;
+            int number2 = 1;
+            int number3 = 9;
+
             Console.WriteLine(" > Input First Number");
             int guess1 = int.Parse(Console.ReadLine());
             Console.WriteLine(" > Input Second Number");
@@ -25,6 +30,39 @@ namespace Baseball
             Console.WriteLine(guess1);
             Console.WriteLine(guess2);
             Console.WriteLine(guess3);
+
+            int strikeCount = 0;
+            int ballCount = 0;
+
+            // guess1
+            if(guess1 == number1)
+            {
+                strikeCount = strikeCount + 1;
+            }
+            else if (guess1 == number2 || guess1 == number3)
+            {
+                ballCount = ballCount + 1;
+            }
+
+            // guess2
+            if (guess2 == number2)
+            {
+                strikeCount = strikeCount + 1;
+            }
+            else if (guess2 == number1 || guess2 == number3)
+            {
+                ballCount = ballCount + 1;
+            }
+
+            // guess3
+            if (guess3 == number3)
+            {
+                strikeCount = strikeCount + 1;
+            }
+            else if (guess3 == number2 || guess3 == number1)
+            {
+                ballCount = ballCount + 1;
+            }
         }
     }
 }
