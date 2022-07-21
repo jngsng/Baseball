c379f75bfae7f41921ac0c06caafa99d28eefe04
JongSeung Kim <jngsngkm@gmail.com>
2022-07-21 22:16:38 +0900

    Input Numbers

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -10,7 +10,21 @@ namespace Baseball
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("=========================BASEBALL=========================");
+            Console.WriteLine("==========================================================");
 
+            Console.WriteLine(" > Input First Number");
+            int guess1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" > Input Second Number");
+            int guess2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" > Input Third Number");
+            int guess3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" > Input Numbers");
+            Console.WriteLine(guess1);
+            Console.WriteLine(guess2);
+            Console.WriteLine(guess3);
         }
     }
 }
