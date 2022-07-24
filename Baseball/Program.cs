6f88ffe78a564ffb12f3f7d99dcf1d38e6d9cfb0
JongSeung Kim <jngsngkm@gmail.com>
2022-07-24 22:40:56 +0900

    반복문 추가

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -20,64 +20,77 @@ namespace Baseball
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers[2]);
 
-            Console.WriteLine(" > Input First Number");
-            int guess1 = int.Parse(Console.ReadLine());
-            Console.WriteLine(" > Input Second Number");
-            int guess2 = int.Parse(Console.ReadLine());
-            Console.WriteLine(" > Input Third Number");
-            int guess3 = int.Parse(Console.ReadLine());
+            int[] guesses = new int[3];
 
-            if(guess1 == guess2 || guess2 == guess3 || guess1 == guess3)
+            while (true)
             {
-                Console.WriteLine("Same Number");
+                Console.WriteLine(" > Input First Number");
+                guesses[0] = int.Parse(Console.ReadLine());
+                Console.WriteLine(" > Input Second Number");
+                guesses[1] = int.Parse(Console.ReadLine());
+                Console.WriteLine(" > Input Third Number");
+                guesses[2] = int.Parse(Console.ReadLine());
+
+                if (guesses[0] == guesses[1] || guesses[1] == guesses[2] || guesses[0] == guesses[3])
+                {
+                    Console.WriteLine("Same Number");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(" > Input Numbers");
+                    Console.WriteLine(guesses[0]);
+                    Console.WriteLine(guesses[1]);
+                    Console.WriteLine(guesses[2]);
+
+                    int strikeCount = 0;
+                    int ballCount = 0;
+
+                    // guess1
+                    if (guesses[0] == numbers[0])
+                    {
+                        strikeCount = strikeCount + 1;
+                    }
+                    else if (guesses[0] == numbers[1] || guesses[0] == numbers[2])
+                    {
+                        ballCount = ballCount + 1;
+                    }
+
+                    // guess2
+                    if (guesses[1] == numbers[1])
+                    {
+                        strikeCount = strikeCount + 1;
+                    }
+                    else if (guesses[1] == numbers[0] || guesses[1] == numbers[2])
+                    {
+                        ballCount = ballCount + 1;
+                    }
+
+                    // guess3
+                    if (guesses[2] == numbers[2])
+                    {
+                        strikeCount = strikeCount + 1;
+                    }
+                    else if (guesses[2] == numbers[1] || guesses[2] == numbers[0])
+                    {
+                        ballCount = ballCount + 1;
+                    }
+
+                    Console.Write("Strike : ");
+                    Console.WriteLine(strikeCount);
+                    Console.Write("Ball : ");
+                    Console.WriteLine(ballCount);
+                    Console.Write("Out : ");
+                    Console.WriteLine(3 - strikeCount - ballCount);
+
+                    if (guesses[0] == numbers[0] && guesses[1] == numbers[1] && guesses[2] == numbers[2])
+                    {
+                        Console.WriteLine("CORRECT!");
+                        break;
+                    }
+                }
             }
-            else
-            {
-                Console.WriteLine(" > Input Numbers");
-                Console.WriteLine(guess1);
-                Console.WriteLine(guess2);
-                Console.WriteLine(guess3);
 
-                int strikeCount = 0;
-                int ballCount = 0;
-
-                // guess1
-                if (guess1 == numbers[0])
-                {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess1 == numbers[1] || guess1 == numbers[2])
-                {
-                    ballCount = ballCount + 1;
-                }
-
-                // guess2
-                if (guess2 == numbers[1])
-                {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess2 == numbers[0] || guess2 == numbers[2])
-                {
-                    ballCount = ballCount + 1;
-                }
-
-                // guess3
-                if (guess3 == numbers[2])
-                {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess3 == numbers[1] || guess3 == numbers[0])
-                {
-                    ballCount = ballCount + 1;
-                }
-
-                Console.Write("Strike : ");
-                Console.WriteLine(strikeCount);
-                Console.Write("Ball : ");
-                Console.WriteLine(ballCount);
-                Console.Write("Out : ");
-                Console.WriteLine(3 - strikeCount - ballCount);
-            }
         }
     }
 }
